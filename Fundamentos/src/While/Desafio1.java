547c35b85c987cec0215caa8482016f9b9d31ac4
gusfim <contato.gustavomarques@gmail.com>
2022-09-16 19:57:32 +0200

    new

--- Fundamentos/src/While/Desafio1.java
+++ Fundamentos/src/While/Desafio1.java
@@ -1,16 +1,28 @@
 package While;
-
+import java.util.*;
 public class Desafio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int contador = 1;
+		/*int contador = 1;
 		
 		while(contador <= 10) {
 			System.out.printf("i = %d\n",contador);
 			contador++;
 		}
+		*/
+
+		Scanner teclado = new Scanner(System.in);
+		
+		String texto = "";
+		
+		while(!texto.equalsIgnoreCase("sair")) {
+			System.out.println("Digite algo: ");
+			texto = teclado.next();
+		}
+		
+		teclado.close();
 	}
 
 }
