53b19d2c0d7abc3aa5cbbc2b57c818e04d9b08ce
gusfim <contato.gustavomarques@gmail.com>
2022-09-16 19:48:56 +0200

    while and for

---
+++ Fundamentos/src/While/Desafio1.java
@@ -0,0 +1,16 @@
+package While;
+
+public class Desafio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int contador = 1;
+		
+		while(contador <= 10) {
+			System.out.printf("i = %d\n",contador);
+			contador++;
+		}
+	}
+
+}
---
+++ Fundamentos/src/While/For.java
@@ -0,0 +1,16 @@
+package While;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(/*declaração da variavel*/int contador = 0; 
+				/*expressao */ contador <= 20; 
+				/*manipulação, neste caso incremento*/contador++) {
+			System.out.printf("i = %d\n", contador);
+		}
+
+	}
+
+}
