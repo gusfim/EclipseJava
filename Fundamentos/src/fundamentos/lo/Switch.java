194cf5c00b516db37be544f0d975d6961e01239e
gusfim <contato.gustavomarques@gmail.com>
2022-09-19 12:07:14 +0200

    switch

---
+++ Fundamentos/src/fundamentos/lo/Switch.java
@@ -0,0 +1,35 @@
+package fundamentos.lo;
+import java.util.*;
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		@SuppressWarnings("resource")
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite a cor da faixa");
+		
+		String faixa = teclado.next();
+		String cor = "A cor da faixa é: ";
+		
+		switch (faixa.toLowerCase()) {
+		case "preta": {
+			
+		}
+		case "azul":{
+		}
+		break;
+		case "amarela":{
+		}
+		break;
+		case "verde":{
+		}
+		break;
+		case "vermelha":{
+		}
+		break;
+		}
+		System.out.println(cor + faixa);
+	}
+
+}
