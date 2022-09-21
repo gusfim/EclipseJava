b5366b563c0e18303d5d75535483ebbcb1f8c5dd
gusfim <contato.gustavomarques@gmail.com>
2022-09-22 00:34:33 +0200

    Simple result

--- TeacherRita/src/exercap4/ImpostoRenda.java
+++ TeacherRita/src/exercap4/ImpostoRenda.java
@@ -8,52 +8,28 @@ public class ImpostoRenda {
 		//IR = salario * alíquota - dedução
 		
 		Scanner teclado = new Scanner(System.in);
-		double salario = teclado.nextDouble();
+		System.out.println("Digite o salario:");
 		
-		double aliquota;	
+		double salario = teclado.nextDouble();
 		double ir ;
-		double salarioFinal;
 		
 		if(salario <= 1903.98) {
-			System.out.println("Salario Isento " + salario);
+			ir = 0;
 		}
 		else if(salario >= 1903.99 && salario <= 2826.65) {
-			aliquota = (salario * 7.5) / 100;
-			ir = (aliquota - 142.80);
-			salarioFinal = salario - (aliquota - 142.80) ;
-			System.out.println("IR 7.5%");
-			System.out.printf("Salario a receber: $%.2f ",salarioFinal);
-			System.out.printf("\nValor a pagar de IR: " 
-			                             + "%.2f" ,ir);
+			ir = salario * 0.075 - 142.80;
 		}
 		else if(salario >= 2826.66 && salario <= 3751.05) {
-			aliquota = (salario * 15.0) / 100;
-			ir = (aliquota - 354.80);
-			salarioFinal = salario - (aliquota - 354.80) ;
-			System.out.println("IR 15%");
-			System.out.printf("Salario a receber: $%.2f ",salarioFinal);
-			System.out.printf("\nValor a pagar de IR: " 
-			                             + "%.2f" ,ir);
+			ir = salario * 0.15 - 354.80;
 		}
 		else if(salario >= 3751.06 && salario <= 4664.68) {
-			aliquota = (salario * 22.5) / 100;
-			ir = (aliquota - 636.13);
-			salarioFinal = salario - (aliquota - 636.13) ;
-			System.out.println("IR 22.5%");
-			System.out.printf("Salario a receber: $%.2f ",salarioFinal);
-			System.out.printf("\nValor a pagar de IR: " 
-			                             + "%.2f" ,ir);
+			ir = salario * 0.225 - 636.13;
 		}
-		
 		else {
-			aliquota = (salario * 27.5) / 100;
-			ir = (aliquota - 869.36);
-			salarioFinal = salario - (aliquota - 869.36) ;
-			System.out.println("IR 27.5%");
-			System.out.printf("Salario a receber: $%.2f ",salarioFinal);
-			System.out.printf("\nValor a pagar de IR: " 
-			                             + "%.2f" ,ir);
+			ir = salario * 0.275 - 869.36;
 		}
+		System.out.println("Valor do salario:" + salario);
+		System.out.printf("Valor do IR: %.2f" , ir);
 		teclado.close();
 	}
 
