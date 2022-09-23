be7d09eca9a20792e0901833ae60aa5a7f2f3892
gusfim <contato.gustavomarques@gmail.com>
2022-09-23 19:44:34 +0200

    used array and args

---
+++ TeacherRita/src/cap5/Array.java
@@ -0,0 +1,22 @@
+package cap5;
+
+public class Array {
+
+	public static void main(String[] args) {
+	
+		for(int x = 0 ; x < args.length ; x++) {
+			System.out.println(args[x]);
+		}
+		/*
+		System.out.println("=================================================================");
+		String nome[] = new String[3];
+		nome[0] = "Gustavo";
+		nome[1] = "Sam";
+		nome[2] = "Kiara";
+		
+		for(int i = 0 ; i < nome.length; i++) {
+			System.out.println(nome[i]);
+		}*/
+	}
+
+}
