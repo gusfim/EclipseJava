3d39f57fc0f0f23659b18b58a6d1573be9edd78b
gusfim <contato.gustavomarques@gmail.com>
2022-10-05 13:15:23 +0200

    Adjustment

--- TeacherRita/src/cap7/ArrayDimension.java
+++ TeacherRita/src/cap7/ArrayDimension.java
@@ -4,19 +4,18 @@ public class ArrayDimension {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		String [][]cidade = {{"Sao Paulo" , "Ribeirao Preto"},
-				{"Minas Gerais", "Diamantina"},
-				{"Curitiba" , "Londrina"}};
-		
-		for(int i = 0; i < cidade.length; i++ ) {
-			for(int j = 0; j < cidade[i].length ; j++) {
-			System.out.println(cidade[i][j]);
+
+		String[][] cidade = {{"Sao Paulo","Ribeirao Preto"},{"Minas Gerais","Diamantina"},
+				             {"Curitiba","Londrina"}};
+
+		for (int i = 0; i < cidade.length; i++) {
+			for (int j = 0; j < cidade[i].length; j++) {
+				System.out.println(cidade[i][j]);
 			}
 		}
 		System.out.println("================================================");
 		for (String[] cidades : cidade) {
-			for(String c: cidades) {
+			for (String c : cidades) {
 				System.out.println(c);
 			}
 		}
